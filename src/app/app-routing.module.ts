72b3f5688ee5d584b376817b51c4f6b921a87f5e
roushan <roushan@stackone.io>
2023-03-11 23:07:34 +0530

    changes for build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { CountryNewsComponent } from './components/country-news/country-news.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
@@ -10,7 +10,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      preloadingStrategy: PreloadAllModules,
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
