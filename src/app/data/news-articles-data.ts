bcd56cf6b92bdc843e229f96a493475e8cdbda8e
roushan <roushan@stackone.io>
2023-03-12 17:10:41 +0530

    added mail and instagram and UI chages

--- src/app/data/news-articles-data.ts
+++ src/app/data/news-articles-data.ts
@@ -9,7 +9,10 @@ export const indiaArticles: Article[] = [
         title: `UK and India in 7th Round of FTA Talks`,
         subTitle: '',
         content: `line with previous rounds, negotiations in the latest round were conducted in a hybrid manner - a number of Indian officials travelled to London for negotiations, while others were able to take part virtually.
-        A total of 43 separate technical sessions were held across 11 policy areas on a variety of technical topics. As part of this program, detailed discussions of draft treaty text were conducted in these policy areas.
+        <br>
+        <br>
+        A total of 43 separate technical sessions were held across 11 policy areas on a variety of technical topics. 
+        As part of this program, detailed discussions of draft treaty text were conducted in these policy areas.
         A round of negotiations is scheduled for later this spring, which will be the eighth round of negotiations.`,
         image: 'in1',
         country: enumCountry.INDIA
@@ -27,18 +30,24 @@ export const turkeyArticles: Article[] = [
         The future is uncertain for those who survived. Finding a safe place to live is one of their most pressing problems. A minimum of 1.5 million people are homeless, and finding them proper shelter will take time.
         Almost two million people have now left the quake zone, according to Afad, the Turkish disaster agency. Others live with friends or family elsewhere in the country. For those who wish to leave the region, flights and trains are free. 
         But Songul and her family aren't leaving the town of Samandag, near the Mediterranean coast. This is very important to us. Whatever happens - even if the house falls down - we will stay here. This is our nest. Everything we have is here. We aren't going anywhere."
-        
+        <br>
+        <br>
+        <br>
         A number of precious pieces of furniture have been carefully removed from the house and placed outside. An image of shells from the Turkish resort of Kusadasi is displayed on top of a polished wooden side table. A large orange sits in a bowl of fruit with white mould growing on it. When things look normal indoors, they seem strange and out of place in the street.
         Currently, the entire family is living in three tents just a few steps from their damaged home. The two of them sleep and eat there, sharing food cooked on a camping stove. There's no proper toilet, although they've recovered one from In a makeshift wooden shed, they are trying to plumbing in the bathroom. A small shower area has even been created. The space and privacy are lacking, and everything is very basic. Overcrowded and cramped tents.
         Songul has been going through an agonizing month. They lost seventeen relatives in the earthquake. She is officially missing her sister Tulay. "We don't know if she is still under the rubble," she tells me. "We don't know if her body has been recovered yet." "We can't start mourning because we haven't evenfound her."  
         The building in Iskenderun where Songul's brother-in-law Husemettin and 11-year-old nephew Lozan were sleeping collapsed around Husemettin and Lozan as they slept during the night.We visited what was left of their home, a sprawling pile of twisted debris. Three blocks of flats had fallen, according to our neighbours.
-        
+        <br>
+        <br>
+        <br>
         Songul says quietly, "We brought Lozan's body here.". His body was brought from the morgue and buried in Samandag. Husemettin was buried in the cemetery of the anonymous.
         A picture of the family smiles out of Tulay's still-active Facebook profile, their arms around each other, faces close. Lozan holds a red balloon tightly.
         The volleyball court in Adana was dotted with blankets and mattresses where families slept. Two trains parked at the railway station in the port city of Iskenderun have become their home. Staff there work hard to keep things clean and tidy, and seats have become beds. Luggage racks are filled with personal possessions. One young girl cries while hugging a pillow instead of a teddy bear. I don't feel at home here.
         Songul's children are also struggling. There are no schools and no toys are trapped inside dangerous houses. Whenever they run out of battery, they go to bed early because they are bored. They just sit around and play with their phones."
         During the night, things become even more difficult. Samandag is without electricity at the moment. Just above the bold UNHCR logo, Songul has draped colourful solar lights across their white tent. Even though they are not refugees, they are still homeless.
-        
+        <br>
+        <br>
+        <br>
         Songul explains that he placed the lamps here so that they would be visible. In the dark, we're scared. No power is a big problem. The fear is too great, and the aftershocks keep us awake. We can't sleep." She wipes her tears away.
         "We are free people, we are used to freedom, independence, and everyone living in their own homes," says her husband, Savas. Currently, three families eat in one tent, live and sit in one tent."
         "We have no idea what the future holds. Our houses have collapsed, what will happen next? We just don't know."`,
@@ -57,21 +66,33 @@ export const americaArticles: Article[] = [
         She became the first wheelchair user to work as a New York City teacher after contracting polio as a child.
         It was announced on Saturday that she had passed away in Washington DC.
         According to a statement posted on her website announcing the death of Heumann, this woman had been widely regarded as 'the mother' of the disability rights movement for decades.
+        <br>
+        <br>
         As part of her activism, she helped initiate laws and established national and international advocacy organizations for disabled people, it said.
+        <br><br>
         ●	US law was changed by the woman who led a 24-day sit-in
+        <br>
         ●	On BBC Sounds, listen to the 25-day sit-in that changed history
+        <br>
+        <br>
         In addition to serving in the Clinton and Obama administrations, Heumann has over 20 years of experience working in the non-profit sector.
         In a tribute to Heumann's lifelong commitment to fighting for civil rights, President Obama called it "fortunate" to work with her.
-        
+        <br>
+        <br>
         AAPWD also paid tribute, saying her leadership had "advanced the rights and inherent dignity of people with disabilities".
-        
+        <br>
+        <br>
         When she was two years old, she contracted polio and was unable to walk. She was born in Philadelphia in 1947 and raised in Brooklyn, New York.
         At age nine, she recounted being treated as a "second-class citizen" when she got into a school because her wheelchair was considered a "fire hazard".
         When she was a child, her parents fought for her rights, and she went on to study speech therapy at Long Island University and earn a master's degree in public health at the University of California, Berkeley.
         In the 1970s, she won a lawsuit against the New York Board of Education and became the first teacher in the state to use a wheelchair.
+        <br>
+        <br>
         Her twenty-four-day sit-in in 1977 helped pave the way for the Americans with Disabilities Act (ADA) in 1990, a law that upheld civil rights for people with disabilities.
         "Disability only becomes a tragedy when society fails to provide us with the things we need to live - such as employment opportunities and accessible buildings," she told a reporter in 1987.
         Despite living in a wheelchair, I do not consider it a tragedy."
+        <br>
+        <br>
         His next position was assistant secretary of special education and rehabilitation services in the Clinton administration from 1993 to 2001 the Department of Education, and was appointed special adviser on International Disability Rights by Barack Obama.
         In addition to the activism she has been engaged in for decades, she has also written a memoir, Being Heumann, as well as a youth version of the book, Rolling Warrior. She was also featured in the award-winning documentary, Crip Camp: A Disability Revolution.
         Heumann is survived by her husband, Jorge, and two brothers, Ricky and Joseph, as well as her parents.`,
@@ -89,8 +110,12 @@ export const australiaArticles: Article[] = [
         subTitle: `A 12-person Australian rapid assessment team has reached Port Vila, Vanuatu, in the wake of last week’s tropical cyclones Judy and Kevin. (Max Bree/Defence)`,
         content: `In response to last week's tropical cyclones Judy and Kevin, a 12-person rapid assessment team has arrived in Port Vila, Vanuatu. ADF personnel from HMAS Canberra, with more than 600 members, were also deployed on Sunday to the island nation.
         In response to the cyclone, the rapid assessment group will travel on two Royal Australian Air Force (RAAF) aircraft. Aerial damage assessments will be carried out, as well as disaster, health, infrastructure, power, and humanitarian assessments.
+        <br>
+        <br>
         Last week, Australian defence minister Richard Marles, foreign minister Penny Wong, minister for international development and the Pacific Pat Conroy, and Aid teams from France and New Zealand are supporting Vanuatu's official response, Aid teams from France and New Zealand are supporting Vanuatu's official response, according to minister for emergency management Murray WattWednesday, Cyclone Judy knocked out power and forced some evacuations.
         Murray Watt, Vanuatu's minister of emergency management, said New Zealand and French assistance were being provided
+        <br>
+        <br>
         As part of Australia's long standing disaster preparedness and humanitarian assistance activities in Vanuatu and elsewhere in the region, Senator Conroy said Australia would be prepared to help.
         Vanuatu's people are in our thoughts at this difficult time. The Australian government stands by the Pacific family - now and well into the future," Conroy said.
         During times of crisis, Australia can be trusted by the Pacific family."`,
@@ -101,4 +126,4 @@ export const australiaArticles: Article[] = [
 
 ];
 
-export const allNewsArticles: Article[] = [...indiaArticles, ...turkeyArticles, ...australiaArticles, ...americaArticles];
+export const allNewsArticles: Article[] = [...indiaArticles, ...americaArticles, ...turkeyArticles, ...australiaArticles];
